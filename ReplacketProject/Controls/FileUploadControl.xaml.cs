1279c102d162230a8bdaccb65cf0b2248d9ed9dc
ariel levi <ariel.levi.2007@gmail.com>
2026-07-26 10:28:34 +0300

    feat: added file upload UserControl

---
+++ ReplacketProject/Controls/FileUploadControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReplacketProject.Controls
+{
+    /// <summary>
+    /// Interaction logic for FileUploadControl.xaml
+    /// </summary>
+    public partial class FileUploadControl : UserControl
+    {
+        public FileUploadControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
